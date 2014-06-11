d6e9a7f59a249865fd04d3e3ef2afa82e2be4089
Jake Townsend <jake@jigme.co.uk>
2014-06-11 08:57:23 +0100

    Initial code written during exercise in Bangalore

---
+++ Roman Numeral Bangalore/KATA.RomanNumeral.UnitTests/ArabicToRomanTests.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KATA.RomanNumeral.Application.Interface;
+using KATA.RomanNumeral.Application.Service;
+using KATA.RomanNumeral.Domain.Interface;
+using KATA.RomanNumeral.Domain.Service;
+
+namespace KATA.RomanNumeral.UnitTests
+{
+	[TestClass]
+	public class ArabicToRomanTests
+	{
+		IArabicToRomanApplicationService arabicToRomanApplicationService;
+
+		[TestInitialize]
+		public void TestInitializer()
+		{
+			IArabicToRomanDomainService arabicToRomanDomainService = new ArabicToRomanDomainService();
+			arabicToRomanApplicationService = new ArabicToRomanApplicationService(arabicToRomanDomainService);
+		}
+
+
+		# region Scope Of Application
+		[TestMethod]
+		[ExpectedException(typeof(IndexOutOfRangeException))]
+		public void test_value_greater_than_3000()
+		{
+			var input = 3001;
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(IndexOutOfRangeException))]
+		public void test_value_less_than_1()
+		{
+			var input = 0;
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+		}
+		#endregion
+
+
+		# region Roman Constant Tests
+		[TestMethod]
+		public void aa_one()
+		{
+			var input = 1;
+			var expectedOutput = "I";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[TestMethod]
+		public void ab_five()
+		{
+			var input = 5;
+			var expectedOutput = "V";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[TestMethod]
+		public void ac_ten()
+		{
+			var input = 10;
+			var expectedOutput = "X";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[TestMethod]
+		public void ad_fifty()
+		{
+			var input = 50;
+			var expectedOutput = "L";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[TestMethod]
+		public void ae_one_hundred()
+		{
+			var input = 100;
+			var expectedOutput = "C";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[TestMethod]
+		public void af_five_hundred()
+		{
+			var input = 500;
+			var expectedOutput = "D";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[TestMethod]
+		public void ag_one_thousand()
+		{
+			var input = 1000;
+			var expectedOutput = "M";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		#endregion
+
+
+
+		#region RomanNonStandards
+		[TestMethod]
+		public void ba_two()
+		{			
+			var input = 2;
+			var expectedOutput = "II";			
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);			
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[TestMethod]
+		public void bb_three()
+		{
+			var input = 3;
+			var expectedOutput = "III";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+
+		[TestMethod]
+		public void bc_four()
+		{			
+			var input = 4;
+			var expectedOutput = "IV";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);			
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+
+		[TestMethod]
+		public void bd_six()
+		{
+			var input = 6;
+			var expectedOutput = "VI";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+
+		[TestMethod]
+		public void be_seven()
+		{
+			var input = 7;
+			var expectedOutput = "VII";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[TestMethod]
+		public void bf_eight()
+		{
+			var input = 8;
+			var expectedOutput = "VIII";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[TestMethod]
+		public void bg_nine()
+		{
+			var input = 9;
+			var expectedOutput = "IX";
+			var result = arabicToRomanApplicationService.GetRomanValueFrom(input);
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		#endregion
+	}
+}
---
+++ Roman Numeral Bangalore/KATA.RomanNumeral/Domain/Service/ArabicToRomanDomainService.cs
@@ -0,0 +1,58 @@
+﻿using KATA.RomanNumeral.Domain.Enum;
+using KATA.RomanNumeral.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace KATA.RomanNumeral.Domain.Service
+{
+	public class ArabicToRomanDomainService : IArabicToRomanDomainService
+	{
+
+		public string GetRomanValue(int input)
+		{
+			string result = string.Empty;
+			
+			if (input > 3000 || input < 1)
+			{
+				throw new IndexOutOfRangeException();
+			}
+
+			if (input <= 3 && input > 1)
+			{
+				for (int i = 1; i <= input; i++)
+				{
+					result += GetRomanConstant(1);
+				}
+
+				return result;
+			}
+
+
+			if(input == 4)
+			{
+				return String.Format(CultureInfo.InvariantCulture, "{0}{1}",((RomanConstant)1).ToString(), ((RomanConstant)5).ToString());			
+			}
+
+			if(input > 5 && input < 9)
+			{
+				return String.Format(CultureInfo.InvariantCulture, "{0}{1}", ((RomanConstant)5).ToString(), GetRomanValue(input - 5));			
+			}
+
+			if(input == 9)
+			{
+				return String.Format(CultureInfo.InvariantCulture, "{0}{1}", ((RomanConstant)1).ToString(), ((RomanConstant)10).ToString());			
+			}
+
+			return GetRomanConstant(input);
+		}
+
+		private string GetRomanConstant(int input)
+		{
+			return ((RomanConstant)input).ToString();
+		}
+
+	}
+}
